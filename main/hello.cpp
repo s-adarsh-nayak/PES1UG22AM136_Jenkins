5aab30248aeda243909a7df3e2592d03c931adc1
S Adarsh Nayak <90552863+s-adarsh-nayak@users.noreply.github.com>
2025-03-11 00:15:32 +0530

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout<<"Hello This is jenkins using bore \n 2nd time \n";
+  cout<<"Hello This is jenkins using bore ";
+  cout<<"Here we can observer simultaneous commit and a job gets created in jenkins \n";
   return 0;
 }
